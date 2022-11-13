4c0a4baa7d7461274391537d4d49361b1c9d14a0
dev-apiarist <klesliedev@gmail.com>
2022-11-13 09:25:35 -0500

    removed duplicate routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,6 @@
 import { FarmersDetailsComponent } from './modules/pages/farmers-details/farmers-details.component';
 import { FarmersComponent } from './modules/pages/farmers/farmers.component';
-import{ AboutComponent} from './modules/pages/about/about.component';
+import { AboutComponent } from './modules/pages/about/about.component';
 import { HomeComponent } from './modules/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes, Router } from '@angular/router';
@@ -9,17 +9,15 @@ import { LivestockComponent } from './modules/livestock/livestock.component';
 const routes: Routes = [
   { path: 'home', component: HomeComponent },
 
-
   { path: 'farmers', component: FarmersComponent },
   { path: 'about', component: AboutComponent },
-==
-  { path: 'home/farmers', component: FarmersComponent },
-
 
   { path: 'details', component: FarmersDetailsComponent },
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+
   { path: '**', redirectTo: 'home' },
   { path: 'farmer/:id', component: FarmersDetailsComponent },
-  {path: 'categories/:id', component: LivestockComponent},
+  { path: 'categories/:id', component: LivestockComponent },
 ];
 
 @NgModule({
