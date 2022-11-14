236f62b8879365735d3dcd805b016c51452ac5fd
niolos <niolos2113@gmail.com>
2022-11-14 13:19:15 -0500

    made updates creating farmers detail component. restored the app-routing module

--- src/app/modules/farmers-details/farmers-details.component.ts
+++ src/app/modules/farmers-details/farmers-details.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { FarmerDetailsService } from 'src/app/services/farmer-details.service';
 import { farmerDetails } from 'src/app/models/farmerDetails';
+import { ProductsService } from 'src/app/services/products.service';
+import { products } from 'src/app/models/products';
 
 @Component({
   selector: 'app-farmers-details',
@@ -10,10 +12,14 @@ import { farmerDetails } from 'src/app/models/farmerDetails';
 })
 export class FarmersDetailsComponent implements OnInit {
 
-  constructor(private FarmerDetalsService: FarmerDetailsService, private Router: Router, private route:ActivatedRoute) { }
+  constructor(private FarmerDetalsService: FarmerDetailsService, private Router: Router, private route:ActivatedRoute, private ProductsService: ProductsService) { }
 
   getFarmer!: farmerDetails
+  getProduct!: products
   waiting:boolean = true
+  public allProducts: Array<any>=[]
+
+
   ngOnInit(): void {
     let id = this.route.snapshot.params["id"];
     console.log(id)
@@ -24,7 +30,19 @@ export class FarmersDetailsComponent implements OnInit {
     this.FarmerDetalsService.getFarmerById(id).subscribe(farmer=>{
       this.getFarmer = farmer.data;
       this.waiting = false
-      console.log(farmer)
+      for(let counter = 0; counter < this.getFarmer.products.length; counter++){
+        // console.log(this.getFarmer.products[counter])
+        let id = this.getFarmer.products[counter]
+        this.getProductById(id)
+      }
+      
     })
   }
+
+  getProductById(id:string){
+    this.ProductsService.getProductById(id).subscribe(prod=>{
+      this.allProducts.push(prod.data);
+    })
+  }
+
 }
